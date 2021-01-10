b67a8fead4974975a28a2249a0531ff7be69231f
Jelte Boelens <j.w.r.boelens@gmail.com>
2021-01-10 17:26:52 +0100

    initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+/**
+ * The container for the application.
+ *
+ * @return {JSX.Element} JSX containing the application.
+ */
+export default function App(): JSX.Element {
+  return (
+    <div>
+      Hello World!
+    </div>
+  );
+}
---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [new ESLintPlugin({
+    extensions: ['ts', 'tsx'],
+  })],
+};
